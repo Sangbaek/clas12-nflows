36d254e6c97de962019ab698059c7d7e776afa98
Sangbaek <sangbaek@mit.edu>
2021-05-16 15:45:40 -0400

    fix a typo

--- utils/manual_split.py
+++ utils/manual_split.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     with open('data/pi0_cartesian.pkl', 'rb') as f2:
         xz2 = np.array(pickle.load(f2), dtype=np.float64)
     dfxz2 = pd.DataFrame(xz2)
-    train,test = split_data(dfxz)
+    train,test = split_data(dfxz2)
     train.to_pickle("data/pi0_cartesian_train.pkl")
     test.to_pickle("data/pi0_cartesian_test.pkl")

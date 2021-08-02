b7f68462ac84440b3a4f9d388081126d5b8a7482
Sangbaek <sangbaek@mit.edu>
2021-08-02 15:56:23 -0400

    save per 100

--- inverse.py
+++ inverse.py
@@ -98,96 +98,96 @@ truth_validation = validation.truth.detach().numpy()
 reco_validation = validation.reco.detach().numpy()
 print("done with reading validation data")
 
-truths_guess = []
 trials = 10000 #Number of overall loops
-
-
 
 means = np.mean(reco_entire - truth_entire, axis = 1)
 stds = np.std(reco_entire - truth_entire, axis = 1)
 
-n_sample = len(reco_validation)#1000
+n_sample = 100
+n_loop = len(reco_validation)//n_sample
 
-for i in range(n_sample):
+for loop_num in range(n_loop):
+    print("new loop "+str(loop_num))
 
-	if i%1000:
-		print("inversing ", i, "th event")
+	truths_guess = []
 
-	maxtruth_e = []
-	logprob_e = []
-	maxtruth_p = []
-	logprob_p = []
-	maxtruth_g = []
-	logprob_g = []
+	for i in range(n_sample):
 
-	#electron
-	reco_e = reco_validation[i:i+1, [0,1,2]]
-	mean_e = means[[0,1,2]]
-	std_e = stds[[0,1,2]]
-	#proton
-	reco_p = reco_validation[i:i+1, [3,4,5]]
-	mean_p = means[[3,4,5]]
-	std_p = stds[[3,4,5]]
-	#photon
-	reco_g = reco_validation[i:i+1, [6,7,8]]
-	mean_g = means[[6,7,8]]
-	std_g = stds[[6,7,8]]
-	
-	truth_e = reco_e + np.random.normal(mean_e, std_e, (trials, 3))
-	truth_p = reco_p + np.random.normal(mean_p, std_p, (trials, 3))
-	truth_g = reco_g + np.random.normal(mean_g, std_g, (trials, 3))
-	
-	# for reco in reco_e:
-	reco_useful = np.tile(reco_e, (trials, 1))
-	reco_useful = torch.tensor(reco_useful, dtype=torch.float32).to(device)
-	truth_e = torch.tensor(truth_e, dtype=torch.float32).to(device)
-	logprob = flow_e.log_prob(inputs=reco_useful,context=truth_e)
-	ind_max = np.argmax(logprob.cpu().detach().numpy())
-	maxtruth_e.append(truth_e[ind_max:ind_max+1, :])
-	logprob_e.append(logprob[ind_max])
-	
-	# for reco in reco_p:
-	reco_useful = np.tile(reco_p, (trials, 1))
-	reco_useful = torch.tensor(reco_useful, dtype=torch.float32).to(device)
-	truth_p = torch.tensor(truth_p, dtype=torch.float32).to(device)
-	logprob = flow_p.log_prob(inputs=reco_useful,context=truth_p)
-	ind_max = np.argmax(logprob.cpu().detach().numpy())
-	maxtruth_p.append(truth_p[ind_max:ind_max+1, :])
-	logprob_p.append(logprob[ind_max])
-	
-	# for reco in reco_g:
-	reco_useful = np.tile(reco_g, (trials, 1))
-	reco_useful = torch.tensor(reco_useful, dtype=torch.float32).to(device)
-	truth_g = torch.tensor(truth_g, dtype=torch.float32).to(device)
-	logprob = flow_g.log_prob(inputs=reco_useful,context=truth_g)
-	ind_max = np.argmax(logprob.cpu().detach().numpy())
-	maxtruth_g.append(truth_g[ind_max:ind_max+1, :])
-	logprob_g.append(logprob[ind_max])
-	
-	#electron
-	truth_val_e = maxtruth_e[np.argmax(logprob_e)].cpu().detach().numpy()
-	E_true_e = np.sqrt(truth_val_e[:, 0]**2 + truth_val_e[:, 1]**2  + truth_val_e[:, 2]**2  +  (0.5109989461 * 0.001)**2).reshape((-1, 1))
-	#proton
-	truth_val_p = maxtruth_p[np.argmax(logprob_p)].cpu().detach().numpy()
-	E_true_p = np.sqrt(truth_val_p[:, 0]**2 + truth_val_p[:, 1]**2  + truth_val_p[:, 2]**2 + (0.938272081)**2).reshape((-1, 1))
-	#photon
-	truth_val_g = maxtruth_g[np.argmax(logprob_g)].cpu().detach().numpy()
-	E_true_g = np.sqrt(truth_val_g[:, 0]**2 + truth_val_g[:, 1]**2  + truth_val_g[:, 2]**2).reshape((-1, 1))
-	
-	NF_true = np.hstack( (E_true_e, truth_val_e, E_true_p, truth_val_p, E_true_g, truth_val_g))
-	truths_guess.append(NF_true)
+		maxtruth_e = []
+		logprob_e = []
+		maxtruth_p = []
+		logprob_p = []
+		maxtruth_g = []
+		logprob_g = []
 
-# now = datetime.now()
-# elapsedTime = (now - start )
-# print("Current time is {}".format(now.strftime("%H:%M:%S")))
-# print("Elapsed time is {}".format(elapsedTime))
-# print("Total estimated run time is {}".format(elapsedTime+elapsedTime/i*(max_range+1-i)))
-Truths = np.concatenate(truths_guess)
-df_Truths = pd.DataFrame(Truths)
-df_Truths.to_pickle("gendata/Cond/3features/UMNN/Truths_dvcs.pkl")#.format(num_features,
-#     num_layers,num_hidden_features,training_sample_size,loop_num))
-#print(truths_guess )
-#print(truth_validation[0:n_sample, :])
-#print(reco_validation[0:n_sample, :])
+		#electron
+		reco_e = reco_validation[i+n_sample*loop_num:i+n_sample*loop_num+1, [0,1,2]]
+		mean_e = means[[0,1,2]]
+		std_e = stds[[0,1,2]]
+		#proton
+		reco_p = reco_validation[i+n_sample*loop_num:i+n_sample*loop_num+1, [3,4,5]]
+		mean_p = means[[3,4,5]]
+		std_p = stds[[3,4,5]]
+		#photon
+		reco_g = reco_validation[i+n_sample*loop_num:i+n_sample*loop_num+1, [6,7,8]]
+		mean_g = means[[6,7,8]]
+		std_g = stds[[6,7,8]]
+		
+		truth_e = reco_e + np.random.normal(mean_e, std_e, (trials, 3))
+		truth_p = reco_p + np.random.normal(mean_p, std_p, (trials, 3))
+		truth_g = reco_g + np.random.normal(mean_g, std_g, (trials, 3))
+		
+		# for reco in reco_e:
+		reco_useful = np.tile(reco_e, (trials, 1))
+		reco_useful = torch.tensor(reco_useful, dtype=torch.float32).to(device)
+		truth_e = torch.tensor(truth_e, dtype=torch.float32).to(device)
+		logprob = flow_e.log_prob(inputs=reco_useful,context=truth_e)
+		ind_max = np.argmax(logprob.cpu().detach().numpy())
+		maxtruth_e.append(truth_e[ind_max:ind_max+1, :])
+		logprob_e.append(logprob[ind_max])
+		
+		# for reco in reco_p:
+		reco_useful = np.tile(reco_p, (trials, 1))
+		reco_useful = torch.tensor(reco_useful, dtype=torch.float32).to(device)
+		truth_p = torch.tensor(truth_p, dtype=torch.float32).to(device)
+		logprob = flow_p.log_prob(inputs=reco_useful,context=truth_p)
+		ind_max = np.argmax(logprob.cpu().detach().numpy())
+		maxtruth_p.append(truth_p[ind_max:ind_max+1, :])
+		logprob_p.append(logprob[ind_max])
+		
+		# for reco in reco_g:
+		reco_useful = np.tile(reco_g, (trials, 1))
+		reco_useful = torch.tensor(reco_useful, dtype=torch.float32).to(device)
+		truth_g = torch.tensor(truth_g, dtype=torch.float32).to(device)
+		logprob = flow_g.log_prob(inputs=reco_useful,context=truth_g)
+		ind_max = np.argmax(logprob.cpu().detach().numpy())
+		maxtruth_g.append(truth_g[ind_max:ind_max+1, :])
+		logprob_g.append(logprob[ind_max])
+		
+		#electron
+		truth_val_e = maxtruth_e[np.argmax(logprob_e)].cpu().detach().numpy()
+		E_true_e = np.sqrt(truth_val_e[:, 0]**2 + truth_val_e[:, 1]**2  + truth_val_e[:, 2]**2  +  (0.5109989461 * 0.001)**2).reshape((-1, 1))
+		#proton
+		truth_val_p = maxtruth_p[np.argmax(logprob_p)].cpu().detach().numpy()
+		E_true_p = np.sqrt(truth_val_p[:, 0]**2 + truth_val_p[:, 1]**2  + truth_val_p[:, 2]**2 + (0.938272081)**2).reshape((-1, 1))
+		#photon
+		truth_val_g = maxtruth_g[np.argmax(logprob_g)].cpu().detach().numpy()
+		E_true_g = np.sqrt(truth_val_g[:, 0]**2 + truth_val_g[:, 1]**2  + truth_val_g[:, 2]**2).reshape((-1, 1))
+		
+		NF_true = np.hstack( (E_true_e, truth_val_e, E_true_p, truth_val_p, E_true_g, truth_val_g))
+		truths_guess.append(NF_true)
+
+	now = datetime.now()
+	elapsedTime = (now - start )
+	print("Current time is {}".format(now.strftime("%H:%M:%S")))
+	print("Elapsed time is {}".format(elapsedTime))
+	# print("Total estimated run time is {}".format(elapsedTime+elapsedTime/i*(max_range+1-i)))
+	Truths = np.concatenate(truths_guess)
+	df_Truths = pd.DataFrame(Truths)
+	df_Truths.to_pickle("gendata/Cond/3features/UMNN/Truths_dvcs_{}.pkl").format(loop_num)#num_features,
+	#     num_layers,num_hidden_features,training_sample_size,loop_num))
+	#print(truths_guess )
+	#print(truth_validation[0:n_sample, :])
+	#print(reco_validation[0:n_sample, :])
 print("done")
 quit()
